0e76cce8ecc0ff09acf8496f23654e1cfdeffd7c
Pranav Kulkarni <121322862+PranavKulkarni33@users.noreply.github.com>
2023-12-23 23:11:28 -0500

    First Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'animeproject-79a48',
+    appId: '1:153845614922:web:6aba16b9926ca837b2d9b3',
+    storageBucket: 'animeproject-79a48.appspot.com',
+    apiKey: 'AIzaSyDUj30W5ZmJZhWmZAezNUBM-hpHFgiPkgQ',
+    authDomain: 'animeproject-79a48.firebaseapp.com',
+    messagingSenderId: '153845614922',
+    measurementId: 'G-2MQT2J0TY6',
+  },
+};
